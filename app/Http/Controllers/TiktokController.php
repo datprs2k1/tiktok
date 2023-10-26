1bbc070a7771749669a1c0e20707cbfc27021b08
datprs <datprs@gmail.com>
2023-10-26 14:26:57 +0700

    Add ffmpeg

--- app/Http/Controllers/TiktokController.php
+++ app/Http/Controllers/TiktokController.php
@@ -4,12 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use ProtoneMedia\LaravelFFMpeg\FFMpeg\FFProbe;
 
 class TiktokController extends Controller
 {
     public function create(Request $request)
     {
+        return $this->initPost($request);
+    }
+
+    public function initPost(Request $request)
+    {
         $video = $request->file('video');
+        $count = round($video->getSize() / 10000000);
+        $ffprobe = FFProbe::create();
+        $duration = $ffprobe->format($video)->get('duration');
+        $duration = explode(".", $duration)[0];
+
+        $title = $request->get('title');
+        $privacyLevel = $request->get('privacy_level');
+        $disableDuet = $request->get('disable_duet');
+        $disableComment = $request->get('disable_comment');
+        $disableStitch = $request->get('disable_stitch');
+
+        return $duration;
     }
     public function callback(Request $request)
     {

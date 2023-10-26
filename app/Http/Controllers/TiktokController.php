ee68954284f8704a34f008129604a2f74461b26b
datprs <datprs@gmail.com>
2023-10-26 13:55:33 +0700

    Log

--- app/Http/Controllers/TiktokController.php
+++ app/Http/Controllers/TiktokController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class TiktokController extends Controller
 {
     public function create(Request $request)
     {
         $video = $request->file('video');
+    }
+    public function callback(Request $request)
+    {
+        Log::info($request);
+
+        return response()->json("Ok");
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/', [TiktokController::class, 'create']);
+Route::post('/callback', [TiktokController::class, 'callback']);
